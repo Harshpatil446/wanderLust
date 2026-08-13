18d9dbee513873b3544f3aa312048b74ab25d3ab
Harsh Patil <jharshpatil@gmail.com>
2026-08-13 21:34:27 +0530

    listing owner added

--- routes/listing.js
+++ routes/listing.js
@@ -32,7 +32,7 @@ router.get("/", wrapAsync(async (req, res) => {
 //show route
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");
     if (!listing) {
         req.flash("error", "Listing you requested for does not exist!");
         return res.redirect("/listings");
@@ -43,6 +43,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 //create route
 router.post("/", validateListing, isLoggedIn, wrapAsync(async (req, res, next) => {
     let newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success", "New listing Created!");
     res.redirect("/listings");
