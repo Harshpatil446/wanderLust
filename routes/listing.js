76a9716798dc7e85ad0a1f837645f439530381f0
Harsh Patil <jharshpatil@gmail.com>
2026-08-16 20:00:04 +0530

    image upload feature added

--- routes/listing.js
+++ routes/listing.js
@@ -4,16 +4,19 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+const { storage } = require("../cloudConfig.js");
+const multer = require("multer");
+const upload = multer({ storage });
 
 router
     .route("/")
     .get(wrapAsync(listingController.index))
     .post(
         isLoggedIn,
+        upload.single("listing[image]"),
         validateListing,
         wrapAsync(listingController.createListing)
     );
-
 // new route
 router.get("/new", isLoggedIn, listingController.renderNewForm)
 
@@ -22,6 +25,7 @@ router
     .get(wrapAsync(listingController.showListing))
     .put(isLoggedIn,
         isOwner,
+        upload.single("listing[image]"),
         validateListing,
         wrapAsync(listingController.updateListing))
     .delete(isLoggedIn,
